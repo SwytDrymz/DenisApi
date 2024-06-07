f8ac79e1b2bbb0b0dd546836646b499dd0e14a4c
SwytDrymz <165410162+SwytDrymz@users.noreply.github.com>
2024-06-08 01:14:05 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -33,8 +33,6 @@ const recipients = [
 
 app.post('/email', (req, res) => {
     const {name, email, message, mobile, option, additional}  = req.body
-    res.status(200).json({ message: "Požadavek přijat. E-mail bude odeslán." });
-
     client
   .send({
     from: sender,
